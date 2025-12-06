0dc300d15c702ebcf3d8f2f1e0f4ab7704238ae2
Daniiel314 <36395002+Daniiel314@users.noreply.github.com>
2025-12-05 23:11:54 -0600

    endpoint incorrecto

--- src/main/java/com/invoice/api/controller/CtrlCart.java
+++ src/main/java/com/invoice/api/controller/CtrlCart.java
@@ -21,27 +21,31 @@ import com.invoice.api.service.SvcCart;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/cart-item")
+@RequestMapping("/cart-item") // Prefijo base para todos los métodos
 public class CtrlCart {
 
     @Autowired
     private SvcCart svc;
 
+    // 1. Agregar al carrito (POST /cart-item)
     @PostMapping
     public ResponseEntity<ApiResponse> addToCart(@Valid @RequestBody DtoCartItemIn dtoIn) {
         return new ResponseEntity<>(svc.addToCart(dtoIn), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    // 2. Obtener carrito (GET /cart-item/detail)
+    @GetMapping("/detail") 
     public ResponseEntity<List<DtoCartItemOut>> getCart() {
         return new ResponseEntity<>(svc.getCart(), HttpStatus.OK);
     }
 
+    // 3. Eliminar item específico (DELETE /cart-item/{id})
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> removeFromCart(@PathVariable("id") Integer id) {
         return new ResponseEntity<>(svc.removeFromCart(id), HttpStatus.OK);
     }
 
+    // 4. Vaciar carrito (DELETE /cart-item)
     @DeleteMapping
     public ResponseEntity<ApiResponse> clearCart() {
         return new ResponseEntity<>(svc.clearCart(), HttpStatus.OK);
